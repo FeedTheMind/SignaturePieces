fa860eebef1cbee418ce3c96397e48ffccda7fee
FeedTheMind <FeedTheMind@users.noreply.github.com>
2016-11-03 19:43:20 -0400

    Set lineWidth to value from brush size input

--- client/javascript/app.js
+++ client/javascript/app.js
@@ -1,7 +1,7 @@
 'use strict';
-var color = $(".selected").css("background-color");
-var $canvas = $("canvas");
-var context = $canvas[0].getContext("2d");
+var color = $('.selected').css('background-color');
+var $canvas = $('canvas');
+var context = $canvas[0].getContext('2d');
 var lastEvent;
 var mouseDown = false;
 var canvas = document.getElementById('clear');
@@ -9,39 +9,39 @@ var $removeColor = $('#removeSelected');
 var $newFact = $('#randomFact');
 
 // When clicking on control list items,
-$(".controls").on("click", "li", function(){
+$('.controls').on('click', 'li', function(){
   // deselects sibling elements or
-  $(this).siblings().removeClass("selected");
+  $(this).siblings().removeClass('selected');
   // selects clicked element
-  $(this).addClass("selected");
+  $(this).addClass('selected');
   // Caches current color
-  color = $(this).css("background-color");
+  color = $(this).css('background-color');
 });
   
-// When "Create Color" is clicked,
-$("#revealColorSelect").click(function(){
+// When 'Create Color' is clicked,
+$('#revealColorSelect').click(function(){
   // shows or hides the color select
   changeColor();
-  $("#colorSelect").slideToggle(250);
+  $('#colorSelect').slideToggle(200);
 });
 
 // Updates the new color span
 function changeColor() {
-  var r = $("#red").val();
-  var g = $("#green").val();
-  var b = $("#blue").val();
-  $("#newColor").css("background-color", "rgb(" + r + "," + g +", " + b + ")");
+  var r = $('#red').val();
+  var g = $('#green').val();
+  var b = $('#blue').val();
+  $('#newColor').css('background-color', 'rgb(' + r + ',' + g +', ' + b + ')');
 }
 
 // When color sliders change
-$("input[type=range]").change(changeColor);
+$('input[type=range]').change(changeColor);
 
-// When "Add Color" is pressed
-$("#addNewColor").click(function(){
+// When 'Add Color' is pressed
+$('#addNewColor').click(function(){
   // append the color to the controls ul
-  var $newColor = $("<li></li>");
-  $newColor.css("background-color", $("#newColor").css("background-color"));
-  $(".controls ul").append($newColor);
+  var $newColor = $('<li></li>');
+  $newColor.css('background-color', $('#newColor').css('background-color'));
+  $('.controls ul').append($newColor);
   // Selects the new color
   $newColor.click();
 });
@@ -53,11 +53,16 @@ $canvas.mousedown(function(e){
 }).mousemove(function(e){
   // draws lines
   if(mouseDown) {
+    // increases size of brush by setting to range's value
+    context.lineWidth = $('#brushSize').val();
+    // 'round' prevents jagged lines
+    context.lineCap = 'round';
     context.beginPath();
     context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
     context.lineTo(e.offsetX, e.offsetY);
     context.strokeStyle = color;
     context.stroke();
+    
     lastEvent = e;
   }
 }).mouseup(function(){
@@ -72,19 +77,20 @@ document.getElementById('clearCanvas').addEventListener('click', function() {
 });
 
 // Removes selected elements  
-$removeColor.click(function(){
-  $(".selected").remove();
+$removeColor.click(function() {
+  $('.selected').remove();
+  // Applies class to first element in unordered list
+  $('.controls ul li').first().addClass('selected');
 });
 
 // Alerts a random fact from the database
-
 function generateNumber() {
   return Math.floor((Math.random() * 6));
 }
 
 $newFact.click(function() {
   var ourIndex = generateNumber();  
-  $.ajax({url: "/fact/" + ourIndex, success: function(result) {
+  $.ajax({url: '/fact/' + ourIndex, success: function(result) {
    alert(result);
   }})
 });
